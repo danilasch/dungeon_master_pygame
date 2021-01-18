57f8e87afaa78ccf6972a198c7dd313a9c632afd
Alenur <75495039+Alenur@users.noreply.github.com>
2021-01-18 19:02:44 +0300

    Add files via upload

---
+++ data.py
@@ -0,0 +1,36 @@
+import pygame
+import os
+
+pygame.init()
+
+def load_image(name):
+    fullname = os.path.join('data', 'pics', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        # print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
+
+def sound(name, volume=1):
+    fullname = os.path.join('data', 'sounds', name)
+    result = pygame.mixer.Sound(fullname)
+    result.set_volume(volume)
+    
+    return result
+
+#sounds
+button_sound = sound('button.wav', 0.5)
+background_music = sound('background.wav', 0.1)
+
+#pics
+inactive_btn = load_image('inactive_button.png')
+active_btn = load_image('active_button.png')
+inactive_box = load_image('inactive_checkbox.png')
+active_box = load_image('active_checkbox.png')
+background = load_image('background.jpg')
+main_cursor = os.path.join('data', 'pics', 'cursor.png')
+
+#fonts
+main_font = os.path.join('data', 'fonts', 'main_font.ttf')
+head_font = os.path.join('data', 'fonts', 'head_font.ttf')
